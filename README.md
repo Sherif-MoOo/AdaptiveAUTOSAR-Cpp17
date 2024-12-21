5c2554a190a454dda25f4863de06448a60166760
Sherif-MoOo <mosherif00111@gmail.com>
2024-12-22 01:26:14 +0200

    Initial commit: Add README and main.cpp

---
+++ README.md
@@ -0,0 +1,2 @@
+# Adaptive Autosar C++17 Project
+This is a simple open-source Adaptive Autosar demo using C++17.
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello from Adaptive Autosar C++17!" << std::endl;
+    return 0;
+}
